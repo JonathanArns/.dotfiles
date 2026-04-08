6ccfe00f3ed1a184391bb971a93aa3edbde21ab2
JonathanArns <jonathan.arns@googlemail.com>
2026-04-08 09:19:45 +0200

    nvim: min supported version 0.12 (use builtin undotree)

--- .config/nvim/lua/keymap.lua
+++ .config/nvim/lua/keymap.lua
@@ -26,8 +26,8 @@ vim.keymap.set('v', 'P', 'p')
 vim.keymap.set('v', 'p', '"_dP') -- don't yank replaced text after paste in visual mode
 
 -- undotree
--- vim.cmd("packadd nvim.undotree")
--- vim.keymap.set('n', '<leader>u', require('undotree').open)
+vim.cmd("packadd nvim.undotree")
+vim.keymap.set('n', '<leader>u', require('undotree').open)
 
 -- quickfix & location list
 vim.keymap.set('n', '<leader>cn', ':cnext<CR>zzzv')
